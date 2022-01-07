e25e5592b334a2df120de4f22d6c82ca5684a69e
jonrot1906 <rotter.jonas@gmail.com>
2022-01-07 23:27:59 +0100

    Create reconnect_sim7000.sh

---
+++ reconnect_sim7000.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+while true; do
+
+        ping -I ppp0 -c 1 8.8.8.8 -s 0
+        i=0
+        if [ $? -eq 0 ]; then
+                echo "Connection up, reconnect not required..."
+                i=0
+        else
+                echo "Connection down, reconnecting..."
+                if [ $i -lt 5 ]; then
+                        sudo ifconfig wwan0 down
+                        sudo pon
+                elif [ $i -ge 5 ] && [ $i -lt 8 ]; then
+                        echo AT+CPOWD=1 | socat - /dev/ttyUSB3,crnl
+                        sleep 5
+                        sudo ifconfig wwan0 down
+                        sudo pon
+                else
+                        echo AT+CPOWD=1 | socat - /dev/ttyUSB3,crnl
+                        sleep 5
+                        gpio write 4 1
+                        sleep 2
+                        gpio write 4 0
+                        sleep 10
+                        sudo ifconfig wwan0 down
+                        sudo pon
+                fi
+                i++
+        fi
+        sleep 10
+done
