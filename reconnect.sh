4d2eb1b2e38f0ad98e58ebec194eb9d1b5d5a27e
jonrot1906 <rotter.jonas@gmail.com>
2022-01-17 10:47:11 +0100

    Update reconnect.sh

--- reconnect.sh
+++ reconnect.sh
@@ -1,6 +1,6 @@
 function restart_power()
 {
-	debug "Power of the module is restarting..."
+	print "Power of the module is restarting..."
 	# Restart power
 	sudo ifconfig wwan0 down
         gpio mode 7 out
@@ -20,27 +20,28 @@ while true; do
     PINGG=$?
 
     if [[ $PINGG -eq 0 ]]; then
-        printf "."
+        print "."
     else
-        printf "/"
+        print "/"
         sleep 10
 	    # Checking cellular internet connection
         ping -q -c 1 -s 0 -w $PING_TIMEOUT -I ppp0 8.8.8.8 > /dev/null 2>&1
         PINGG=$?
 
         if [[ $PINGG -eq 0 ]]; then
-            printf "+"
+            print "+"
         else
 	    debug "Connection is down, reconnecting..."      
 	    restart_power
 	    sudo pon
+	    sudo /etc/init.d/rinetd start
 			
-			# check default interface
-			route | grep ppp | grep default > /dev/null
-			PPP_IS_DEFAULT=$?
-			if [[ $PPP_IS_DEFAULT -ne 0 ]]; then sudo route add default ppp0; debug "ppp0 is added as default interface manually."; fi
-	    fi
+	    # check default interface
+	    route | grep ppp | grep default > /dev/null
+	    PPP_IS_DEFAULT=$?
+	    if [[ $PPP_IS_DEFAULT -ne 0 ]]; then sudo route add default ppp0; print "ppp0 is added as default interface manually."; fi
+	fi
       
     fi
-	sleep 60
+    sleep 60
 done
