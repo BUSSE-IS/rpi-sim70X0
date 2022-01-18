e02627607f955e04a4811ecf07b69d7261eb875b
jonrot1906 <rotter.jonas@gmail.com>
2022-01-18 18:43:44 +0100

    Update reconnect.sh

--- reconnect.sh
+++ reconnect.sh
@@ -20,20 +20,20 @@ while true; do
     PINGG=$?
 
     if [[ $PINGG -eq 0 ]]; then
-        print "."
+        echo "."
 	i=0
     else
-        print "/"
+        echo "/"
         sleep 10
 	    # Checking cellular internet connection
         ping -q -c 1 -s 0 -w $PING_TIMEOUT -I ppp0 8.8.8.8 > /dev/null 2>&1
         PINGG=$?
 
         if [[ $PINGG -eq 0 ]]; then
-            print "+"
+            echo "+"
 	    i=0
         elif [[ $PINGG -eq 0 ]] && [[ $i -le 10 ]]; then
-	    print "Connection is down, reconnecting..."      
+	    echo "Connection is down, reconnecting..."      
 	    restart_power
 	    sudo ifconfig wwan0 down
 	    sudo pon
@@ -42,10 +42,10 @@ while true; do
 	    # check default interface
 	    route | grep ppp | grep default > /dev/null
 	    PPP_IS_DEFAULT=$?
-	    if [[ $PPP_IS_DEFAULT -ne 0 ]]; then sudo route add default ppp0; print "ppp0 is added as default interface manually."; fi
+	    if [[ $PPP_IS_DEFAULT -ne 0 ]]; then sudo route add default ppp0; echo "ppp0 is added as default interface manually."; fi
 	    ((i=i+1))
 	elif [[ $PINGG -eq 0 ]] && [[ $i -gt 10 ]]; then
-	    print "Reboot indicated because too many reconnect failures."
+	    echo "Reboot indicated because too many reconnect failures."
 	    sleep 5
 	    sudo shutdown -r now
 	fi
