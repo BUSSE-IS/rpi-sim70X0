a07a27effed9e16adb069593c66cf0cd5673c662
jonrot1906 <rotter.jonas@gmail.com>
2022-01-22 12:55:25 +0100

    Update reconnect.sh

--- reconnect.sh
+++ reconnect.sh
@@ -1,8 +1,8 @@
 function restart_power()
 {
-	echo "Power of the module is restarting..."
-	# Restart power
-	sudo ifconfig wwan0 down
+        echo "Power of the module is restarting..."
+        # Restart power
+        sudo ifconfig wwan0 down
         gpio mode 7 out
         gpio -g write 7 1
         sleep 1.5
@@ -16,40 +16,39 @@ function restart_power()
 i=0
 while true; do
     # Checking cellular internet connection
-    ping -q -c 1 -s 0 -w $PING_TIMEOUT -I ppp0 8.8.8.8 > /dev/null 2>&1
+    ping -q -c 1 -s 0 -w 2 -I ppp0 8.8.8.8 > /dev/null 2>&1
     PINGG=$?
 
     if [[ $PINGG -eq 0 ]]; then
         echo "."
-	i=0
+        i=0
     else
         echo "/"
-        sleep 10
-	    # Checking cellular internet connection
-        ping -q -c 1 -s 0 -w $PING_TIMEOUT -I ppp0 8.8.8.8 > /dev/null 2>&1
+        sleep 2
+            # Checking cellular internet connection
+        ping -q -c 1 -s 0 -w 2 -I ppp0 8.8.8.8 > /dev/null 2>&1
         PINGG=$?
 
         if [[ $PINGG -eq 0 ]]; then
             echo "+"
-	    i=0
+            i=0
         elif [[ $PINGG -ne 0 ]] && [[ $i -le 10 ]]; then
-	    echo "Connection is down, reconnecting..."
-	    sudo poff
-	    restart_power
-	    sudo ifconfig wwan0 down
-	    sudo pon
-	    sudo /etc/init.d/rinetd restart
-			
-	    # check default interface
-	    route | grep ppp | grep default > /dev/null
-	    PPP_IS_DEFAULT=$?
-	    if [[ $PPP_IS_DEFAULT -ne 0 ]]; then sudo route add default ppp0; echo "ppp0 is added as default interface manually."; fi
-	    ((i=i+1))
-	elif [[ $PINGG -ne 0 ]] && [[ $i -gt 10 ]]; then
-	    echo "Reboot indicated because too many reconnect failures."
-	    sleep 5
-	    sudo shutdown -r now
-	fi
+            echo "Connection is down, reconnecting..."
+            sudo poff
+            restart_power
+            sudo pon
+            sudo /etc/init.d/rinetd restart
+
+            # check default interface
+            route | grep ppp | grep default > /dev/null
+            PPP_IS_DEFAULT=$?
+            if [[ $PPP_IS_DEFAULT -ne 0 ]]; then sudo route add default ppp0; echo "ppp0 is added as default interface manually."; fi
+            ((i=i+1))
+        elif [[ $PINGG -ne 0 ]] && [[ $i -gt 10 ]]; then
+            echo "Reboot indicated because too many reconnect failures."
+            sleep 5
+            sudo shutdown -r now
+        fi
     fi
     sleep 60
 done
